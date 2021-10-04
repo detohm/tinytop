c1db1ce5c50b2360340909cabb2b48e230cd3b5a
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2021-10-04 19:18:30 +0700

    add ncurse lib and its example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
+#include <ncurses.h>
+#include <unistd.h>
+
 #include <iostream>
 #include <vector>
 
 #include "linux.h"
 #include "process.h"
 #include "system.h"
-
 int main() {
-  std::cout << "Hello from TinyTop" << std::endl;
+  // init ncurse
+  initscr();
+  cbreak();
 
-  LinuxHelper linux;
-  std::vector<Process> ps = linux.ProcessList();
-  for (auto a : ps) {
-    std::cout << a.Pid << std::endl;
-  }
+  int row, col;
+  getmaxyx(stdscr, row, col);
+
+  refresh();
+
+  WINDOW *win = newwin(row, col, 0, 0);
+  box(win, 0, 0);
+  wrefresh(win);
+  refresh();
+
+  getch();
+  endwin();
+
+  // LinuxHelper linux;
+  // std::vector<Process> ps = linux.ProcessList();
+  // for (auto a : ps) {
+  //   std::cout << a.Pid << std::endl;
+  // }
 
   return 0;
 }
