47e097f6533d2d27805c29ea485195df4df2f5b2
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2021-10-19 22:47:57 +0700

    add window layout

--- src/display.cpp
+++ src/display.cpp
@@ -37,11 +37,25 @@ void Display::Render() {
 void Display::renderSystemWindow() {
   int row = 0;
   mvwprintw(sysWindow_, ++row, 2, ("OS: " + system_.OperatingSystem()).c_str());
+  mvwprintw(sysWindow_, ++row, 2, "CPU: ");
+  mvwprintw(sysWindow_, ++row, 2, "Mem: ");
+  mvwprintw(sysWindow_, ++row, 2, "Process[total]: ");
+  mvwprintw(sysWindow_, ++row, 2, "Process[running]: ");
+  mvwprintw(sysWindow_, ++row, 2, "UpTime: ");
+
   wrefresh(sysWindow_);
 }
 
 void Display::renderProcessWindow() {
   int row = 0;
-  mvwprintw(processWindow_, ++row, 2, std::string("PROCESS:").c_str());
+  int col[6] = {2, 9, 16, 26, 38, 48};
+
+  mvwprintw(processWindow_, ++row, col[0], "PID");
+  mvwprintw(processWindow_, row, col[1], "User");
+  mvwprintw(processWindow_, row, col[2], "CPU[%%]");
+  mvwprintw(processWindow_, row, col[3], "Mem[MB]");
+  mvwprintw(processWindow_, row, col[4], "Time");
+  mvwprintw(processWindow_, row, col[5], "CMD");
+
   wrefresh(processWindow_);
 }
